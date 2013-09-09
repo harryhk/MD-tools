7b9cbc5588b54f320c4e310874416e1d3d6e3e20
harryhk <harryhuangkun@gmail.com>
2013-09-09 14:27:22 -0400

    first trial to organize code into subfolds and add README.md

---
+++ common/lnx_util.py
@@ -0,0 +1,42 @@
+# this file provides basic utilities for other analyse tools. 
+import re
+import sys
+import numpy as np 
+
+def parseInput(l):
+    ''' l = sys.argv[1:]. this will parse the input and return a dictionary.
+    '''
+    
+    inputP = {}
+    
+    flag = re.compile('-\D+')
+    indx = [ i for (i, n ) in enumerate(l) if flag.match(n)   ]
+    indx.append(len(l))
+    
+    for i in range( len(indx) -1 ):
+        inputP[l[indx[i]]] = l[ indx[i]+1: indx[i+1] ]
+        if len(inputP[l[indx[i]] ] ) ==1 :
+            inputP[ l[indx[i]]] = inputP[l[indx[i]]][0]
+
+    return inputP
+
+
+class MySet(set):
+    def __repr__(self):
+        return ' '.join( repr(i) for i in self  )
+
+def print_help(inputP, paraOpt, helpdoc):
+    ''' inputP: dictionary parsed from parseInput
+        paraOpt: legit keys in inputP 
+        helpdoc: help string, trigerred by -h  
+    '''
+    tmp = MySet( inputP ) - MySet(paraOpt ) 
+    if inputP.has_key('-h') or  len(tmp) > 0:
+        sys.stderr.write("Invalid input: %s\n" %  repr(tmp) )
+        sys.stderr.write(helpdoc)
+        sys.exit(1)
+
+def xvg( fn  ):
+    f=open(fn, 'r')
+    data = [ i.strip() for i in f  ]
+    return np.array( [  i.split()  for i in data if len(i)!=0 and i[0] !='@' and i[0] != '#' ] , dtype='float')
